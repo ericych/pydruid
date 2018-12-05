8d2d69f6bab86ea53f978119295291facdda4092
Eric Yang <ericyangch@hotmail.com>
2018-12-05 16:20:14 -0500

    Update query.py

--- pydruid/query.py
+++ pydruid/query.py
@@ -440,7 +440,7 @@ class QueryBuilder(object):
         query_type = 'scan'
         valid_parts = [
             'datasource', 'filter', 'columns', 'intervals', 'limit',
-            'batchsize', 'resultformat', 'context'
+            'batchsize', 'resultFormat', 'context'
         ]
         self.validate_query(query_type, valid_parts, args)
         return self.build_query(query_type, args)
